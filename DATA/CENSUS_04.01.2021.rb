88756220bac40914e334b3d71ba6e7684880d343
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-22 16:38:47 -0700

    3 original drafts
    The 3 original drafts for this project in Ruby format.

---
+++ DATA/CENSUS_04.01.2021.rb
@@ -0,0 +1,103 @@
+
+## Operating system census data (as of April 1st 2021)
+
+### Linux Census data
+
+#### Debian/Ubuntu (Census data)
+
+* Ubuntu 20.04.1 devices: 2x
+
+* Lubuntu ?.x devices: 1x
+
+Total: 3
+
+#### Other/Android (Census data)
+
+* Android 8.0 devices: 2x
+
+* Android 10.0 devices: 2x
+
+* Android 11.0 devices: 3x
+
+* FireOS devices: 4x
+
+Total: 11
+
+#### Other/TV (Census data)
+
+* WebOS devices: 3x
+
+* Roku devices: 3x
+
+Total: 6
+
+#### Other/Modified Android (Census data)
+
+* Other/Android (rooted)
+
+* Rooted Android 9.0 devices: 2x
+
+Total: 2
+
+#### Other/unknown (Census data)
+
+* Security camera Linux.unknown devices: 8x
+
+* Security camera Linux2.unknown devices: 14x
+
+* Security camera Linux3.unknown devices: 2x
+
+* Truck Linux.unknown devices: 1x
+
+* Wi-Fi router Linux.unknown: 2x
+
+Total: 27
+
+#### Other/Debian (owned by school) (Census data)
+
+* ChromeOS devices: 1x
+
+Total: 1
+
+#### Virtual machines running Linux (Census data)
+
+* Android x86 2.2 machines: 2x
+
+* Android x86 9.0 machines: 2x
+
+* Fedora 31 machines: 2x
+
+* Fedora 32 machines: 2x
+
+* Ubuntu 20.04 machines: 2x
+
+* Kubuntu 18.04 machines: 2x
+
+Total: 12
+
+Grand total: 62 Linux devices (50 when not counting Virtual Machines)
+
+### Windows Census data
+
+#### Windows 10 devices (Census data)
+
+* Windows 10 Build 1909 devices: 2x (owned by Banner Bank and Providence St. Mary's Medical Center)
+
+* Windows 10 build 2004 devices: 1x
+
+Total: 3
+
+#### Virtual machines running Windows (Census data)
+
+* Windows XP Service pack 2 machines: 2x
+
+* Windows XP Service pack 3 machines: 2x
+
+Total: 4
+
+Grand total: 7 Windows devices (3 when not counting Virtual Machines)
+
+Linux to Windows ratio: 62:7 (when not counting virtual machines: 50:3)
+
+***
+
---
+++ DATA/CENSUS_04.02.2021.rb
@@ -0,0 +1,143 @@
+
+***
+
+## Operating system census data (as of April 2nd 2021)
+
+### Linux Census data
+
+#### Debian/Ubuntu (Census data)
+
+* Ubuntu 20.04.1 devices: 2x
+
+* Lubuntu ?.x devices: 1x
+
+Total: 3
+
+#### Other/Android (Census data)
+
+* Android 7.0 devices: 2x
+
+* Android 8.0 devices: 2x
+
+* Android 10.0 devices: 2x
+
+* Android 11.0 devices: 3x
+
+* FireOS devices: 4x
+
+Total: 13
+
+#### Linux/Printers (Census data)
+
+* HP 2D printer: 1x
+
+* Ender3 3D printer: 2x
+
+Total: 3
+
+#### Other/TV (Census data)
+
+* WebOS devices: 3x
+
+* Roku devices: 3x
+
+Total: 6
+
+#### Other/Modified Android (Census data)
+
+* Other/Android (rooted)
+
+* Rooted Android 9.0 devices: 2x
+
+Total: 2
+
+#### Other/unknown (Census data)
+
+* Security camera Linux.unknown devices: 8x
+
+* Security camera Linux2.unknown devices: 14x
+
+* Security camera Linux3.unknown devices: 2x
+
+* Truck Linux.unknown devices: 1x
+
+* Wi-Fi router Linux.unknown: 2x
+
+Total: 27
+
+#### Other/Debian (owned by school) (Census data)
+
+* ChromeOS devices: 1x
+
+Total: 1
+
+#### Virtual machines running Linux (Census data)
+
+* Android x86 2.2 machines: 2x
+
+* Android x86 9.0 machines: 2x
+
+* Fedora 31 machines: 2x
+
+* Fedora 32 machines: 2x
+
+* Ubuntu 20.04 machines: 2x
+
+* Kubuntu 18.04 machines: 2x
+
+Total: 12
+
+Grand total: 67 Linux devices (55 when not counting Virtual Machines)
+
+### Windows Census data
+
+#### Windows 10 devices (Census data)
+
+* Windows 10 Build 1909 devices: 2x (owned by Banner Bank and Providence St. Mary's Medical Center) '#Keep this here to maintain a "proper" Ruby syntax
+
+* Windows 10 build 2004 devices: 1x
+
+Total: 3
+
+#### Virtual machines running Windows (Census data)
+
+* Windows XP Service pack 2 machines: 2x
+
+* Windows XP Service pack 3 machines: 2x
+
+Total: 4
+
+### BSD Census data
+
+#### BSD/Nintendo
+
+* Nintendo Switch: 2x
+
+* Nintendo 2DS XL: 2x
+
+* Nintendo 3DS: 1x
+
+* Nintendo Wii: 1x
+
+Total: 6
+
+Grand total: 6 BSD devices (6 when not counting Virtual Machines)
+
+### Other
+
+#### Unknown
+
+* Radio Scanner: 3x
+
+* PlayStation 2: 2x
+
+* PlayStation 2 slim: 1x
+
+Grand total: 7 Windows devices (3 when not counting Virtual Machines)
+
+Linux to Windows ratio: 67:7 (when not counting virtual machines: 50:3)
+
+Linux to BSD to Windows ratio: 67:7:6 (when not counting virtual machines: 55:3:6)
+
+***
+
